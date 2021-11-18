93e1b5f97fa352a28346a994b29d73d1d5a8b032
Gemma Zaragoza <gemma.zaragoza.work@gmail.com>
2021-11-18 10:42:41 +0100

    comando revisor hecho

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,18 +2,15 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MakeUserRevisor;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
+
     protected $commands = [
-        //
+        MakeUserRevisor::class
     ];
 
     /**
